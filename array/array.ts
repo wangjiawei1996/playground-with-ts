9133709b26bb6ffe71ba3c5489c586af54fe4bca
王嘉巍 <906789088@qq.com>
2019-04-23 11:56:52 +0800

    define class in ts and using this particular

--- array/array.ts
+++ array/array.ts
@@ -3,18 +3,32 @@
 // }
 // var user = ['1', '2', '3'];
 // document.body.innerHTML = array(user);
-class Student {
-  fullName: string;
-  constructor(public firstName,public lastName,public middleInitial) {
-    this.fullName = firstName + " " + middleInitial + " " + lastName;
-  }
+// class Student {
+//   fullName: string;
+//   constructor(public firstName,public lastName,public middleInitial) {
+//     this.fullName = firstName + " " + middleInitial + " " + lastName;
+//   }
+// }
+// interface Person {
+//   firstName: string;
+//   lastName: string;
+// }
+// function array(person: Person) {
+//   return 'hello,' + person.firstName + '' + person.lastName
+// }
+// var user = new Student("Jane", "M.", "User");
+// document.body.innerHTML = array(user);
+
+// var myname:string = 'jiawei';
+// myname = 123
+// function last(): void {
+//   return ""
+// }
+class Person {
+  name: string;
+  age: number
 }
-interface Person {
-  firstName: string;
-  lastName: string;
-}
-function array(person: Person) {
-  return 'hello,' + person.firstName + '' + person.lastName
-}
-var user = new Student("Jane", "M.", "User");
-document.body.innerHTML = array(user);
+var wangjiawe: Person = new Person()
+
+wangjiawe.name = "王嘉巍";
+wangjiawe.age = 22;

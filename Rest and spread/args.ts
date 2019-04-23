ffbf12e688457bb66c773f8213dffe48737b763e
王嘉巍 <906789088@qq.com>
2019-04-23 21:49:03 +0800

    by rest and spread in ...data explored

---
+++ Rest and spread/args.ts
@@ -0,0 +1,8 @@
+function fun1(...args){
+  args.forEach(function(arg) {
+    console.log(arg)
+  })
+}
+
+fun1(1, 2, 3)
+fun1(7, 8, 9, 10, 11)
--- array/static.ts
+++ array/static.ts
@@ -1,8 +1,8 @@
 var username: string = 'wangjiawei'
 
-function test(a: string,b: string,c: string = 'jaiwei'){
+function test(a: string,b?: string,c: string = 'jaiwei'){
   console.log(a);
   console.log(b);
   console.log(c);
 }
-test('xxxx', 'yyy', 'qqq')
+test('xxxx')

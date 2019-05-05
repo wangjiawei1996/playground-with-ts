27078645f9332d619fb76b6449f6e8ef79b813e0
王嘉巍 <906789088@qq.com>
2019-05-05 18:48:35 +0800

    gulp in 4.x using gulp.series finish npm

---
+++ src/gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+
+// 转译JavaScript
+gulp.task("webpack", () => {
+  const webpack = require("webpack-stream");
+  const config = require("./webpack.config.js")
+  gulp.src("./js/**/*.js")
+    .pipe(webpack(config))
+    .pipe(gulp.dest("../www/js"));
+})
+// 编译 less -> css
+gulp.task("less", () => {
+  const less = require("gulp-less")
+  gulp.src("./less/*.js")
+    .pipe(less())
+    .pipe(gulp.dest("../www/css"))
+})
+gulp.task("default", gulp.series["webpack", "less"])

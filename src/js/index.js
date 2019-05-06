4fa9b953c0aedeb28a08d2a0f0d272a25b04bb42
王嘉巍 <906789088@qq.com>
2019-05-06 23:31:35 +0800

    using math.floor and math.random in table change data

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,6 @@
-function makeRow(v=0) {
-  const array = new Array(9);
-  array.fill(v)
-  return array
-}
-function makeMatrix(v=0) {
-  return Array.from({length: 9}, () => makeRow(v))
-}
-const a = makeMatrix();
-a [0][1] = 2;
+const toolkit = require('./toolkit')
+const matrix = toolkit.makeMatrix();
+console.log(matrix)
+const a = Array.from({length: 9}, (v, i) => i);
 console.log(a)
+console.log(toolkit.shuffle(a))

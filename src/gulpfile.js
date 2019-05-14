790591b3f47aed3db1288d6c971550bf497ff84a
王嘉巍 <906789088@qq.com>
2019-05-14 22:27:59 +0800

    design page using gulp will .less change .css

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,20 +1,26 @@
-const gulp = require("gulp");
+const gulp = require("gulp")
 
 // 转译JavaScript
-gulp.task("webpack", done => {
-  const webpack = require("webpack-stream");
-  const config = require("./webpack.config.js")
-  gulp.src("./js/**/*.js")
-    .pipe(webpack(config))
-    .pipe(gulp.dest("../www/js"));
-  done()
+gulp.task('webpack', done => {
+    const webpack = require('webpack-stream')
+    const config = require('./webpack.config')
+
+    gulp.src('../src/js/**/*.js')
+        .pipe(webpack(config))
+        .pipe(gulp.dest('../www/js'))
+
+    done()
 })
+
 // 编译 less -> css
-gulp.task("less", done => {
-  const less = require("gulp-less")
-  gulp.src("./less/*.js")
-    .pipe(less())
-    .pipe(gulp.dest("../www/css"))
-  done()
+gulp.task('css', done => {
+    const less = require('gulp-less')
+    gulp.src('../src/less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('../www/css'))
+
+    done()
 })
-gulp.task("default", gulp.series("webpack", "less"))
+
+// 顺序执行webpack、css两个任务
+gulp.task('default', gulp.series('webpack', 'css'))

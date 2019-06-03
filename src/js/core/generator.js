1c89962cde473db7e7c9f883084ae92c373450c7
王嘉巍 <906789088@qq.com>
2019-06-03 16:46:39 +0800

    finish generator and tookit get math.random() and fillter data

--- src/js/core/generator.js
+++ src/js/core/generator.js
@@ -1,39 +1,71 @@
 // 生成数独解决方案的方法
 const Toolkit = require('./toolkit')
 class Generator {
-  generator() {
-    this.matrix = Toolkit.matrix.makeMatrix();
-    // TUDO方法
-    for (let n = 1;n <= 9; n++) {
-      this.fillNumber(n);
+
+  generator () {
+    while (!this.internalGenerator()) {
+      // TODO
+      // console.warn('try again');
     }
   }
-  fillNumber(n) {
-    this.fillRow(n, 0)
+
+  internalGenerator () {
+    this.matrix = Toolkit.matrix.makeMatrix();
+    this.orders = Toolkit.matrix.makeMatrix()
+      .map(row => row.map((v, i) => i))
+      .map(row => Toolkit.matrix.shuffle(row));
+
+    // Toolkit.matrix.makeRow()
+    for (let n = 1; n <= 9; n++) {
+      if (!this.fillNumber(n)) {
+        return false;
+      }
+    }
+    return true;
   }
-  fillRow(n, rowIndex) {
+
+  fillNumber (n) {
+    return this.fillRow(n, 0);
+  }
+
+  fillRow (n, rowIndex) {
     if (rowIndex > 8) {
       return true;
     }
+
     const row = this.matrix[rowIndex];
-    for(let i = 0;i < 9;i++) {
-      const colIndex = i;
-      // 如果这个位置有值，跳过
-      if (row[rowIndex]) {
+    // 随机选择列
+    const orders = this.orders[rowIndex];
+
+    for (let i = 0; i < 9; i++) {
+      const colIndex = orders[i];
+
+      // 如果该位置已经有值，则跳过
+      if (row[colIndex]) {
         continue;
       }
-      // 检查这个位置是否可以填 n
-      if (!Toolkit.matrix.checkFileable()) {
+
+      // 检查此位置是否可以填 n （!列和宫都存在）
+      if (!Toolkit.matrix.checkFillable(this.matrix, n, rowIndex, colIndex)) {
         continue;
       }
-      row[rowIndex] = n;
-      // 去下一行填写 n,如果没有填进去，就继续寻找当前行的下一个位置
-      if(!this.fillRow(n, rowIndex + 1)) {
+
+      row[colIndex] = n;
+      // 当前行填写 n 成功，递归调用 fillRow() 在下一行中填写 n
+      // 去下一行填写 n，如果没填进去，就继续寻找当前行的下一个位置
+      if (!this.fillRow(n, rowIndex + 1)) {
         row[colIndex] = 0;
-        continue
+        continue;
       }
+
       return true;
     }
+
     return false;
   }
 }
+
+
+const generator = new Generator();
+generator.generator();
+console.log(generator.matrix);
--- src/js/core/toolkit.js
+++ src/js/core/toolkit.js
@@ -1,45 +1,92 @@
- const matrixToolkit = {
-  makeRow(v=0) {
+const matrixToolkit = {
+  makeRow (v = 0) {
     const array = new Array(9);
-    array.fill(v)
-    return array
+    array.fill(v);
+    return array;
   },
-  makeMatrix(v=0) {
-    return Array.from({length: 9}, () => this.makeRow(v))
+  
+  makeMatrix (v = 0) {
+    return Array.from({length: 9}, () => this.makeRow(v));
   },
-  shuffle(array) {
+  
+  /**
+   * Fisher-Yates 洗牌算法
+   * 对传入数组进行随机排序，然后返回新数组
+   * @param {*} array
+   */
+  shuffle (array) {
     const endIndex = array.length - 2;
-    for(let i = 0; i <= endIndex; i++) {
+    for (let i = 0; i <= endIndex; i++) {
       const j = i + Math.floor(Math.random() * (array.length - i));
-      [array[i], array[j]] = [array[j], array[i]]
+      [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
   },
-  /**
-   * 检查指定位置是否可以填写 n
-   */
-  checkFileable() {
-    return true
-  }
- };
- /**
-  * 宫坐标系工具
-  */
- const boxToolit = {
 
- };
- // 工具集
- module.exports = class Toolkit {
-   /**
-    * 矩阵和数据相关的数据
-    */
-   static get matrix() {
-     return matrixToolkit;
-   }
-   /**
-    * 宫坐标系相关的工具
-    */
-   static get box() {
-     return boxToolit;
-   }
- }
+  /**
+   * TODO 检查指定位置可以填写数字 n
+  */
+  checkFillable (matrix, n, rowIndex, colIndex) {
+    const row = matrix[rowIndex];
+    const col = this.makeRow().map((v, i) => matrix[i][colIndex]);
+    const {boxIndex} = boxToolkit.convertToBoxIndex(rowIndex, colIndex);
+    const box = boxToolkit.getBoxCells(matrix, boxIndex);
+
+    for (let i = 0; i < 9; i++) {
+      if (row[i] === n || col[i] === n || box[i] === n) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+/**
+ * 宫坐标系工具
+*/
+const boxToolkit = {
+  convertToBoxIndex (rowIndex, colIndex) {
+    return {
+      boxIndex: Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3),
+      cellIndex: rowIndex % 3 * 3 + colIndex % 3
+    }
+  },
+
+  convertFromBoxIndex (boxIndex, cellIndex) {
+    return {
+      rowIndex: Math.floor(boxIndex / 3) * 3 + Math.floor(cellIndex / 3),
+      colIndex: boxIndex % 3 * 3 + cellIndex % 3
+    }
+  },
+
+  getBoxCells (matrix, boxIndex) {
+    const startRowIndex = Math.floor(boxIndex / 3) * 3;
+    const startColIndex = boxIndex % 3 * 3;
+    const result = [];
+
+    for (let cellIndex = 0; cellIndex < 9; cellIndex++) {
+      const rowIndex = startRowIndex + Math.floor(cellIndex / 3);
+      const colIndex = startColIndex + cellIndex % 3;
+      result.push(matrix[rowIndex][colIndex]);
+    }
+    return result;
+  }
+}
+
+// 工具集
+
+module.exports = class Toolkit {
+  /**
+   * 矩阵和数据相关工具
+   */
+  static get matrix () {
+    return matrixToolkit;
+  }
+
+  /**
+   * 宫坐标系相关工具
+   */
+  static get box () {
+    return boxToolkit;
+  }
+};
